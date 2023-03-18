dfdd545676e09e3039fb9c677836c55756b3c67f
Noah Jensen <noahj1247@gmail.com>
2023-03-17 22:25:11 -0600

    Changed Files

---
+++ NoahJensenProjectileMotion/Projectile_Motion.py
@@ -0,0 +1,22 @@
+"""This program will calculate the height and also graph the projectile"""
+import numpy as np
+from sympy import *
+
+def getinput():
+    """This function takes in the input and passes the values to the isValid function """
+    height = input("Please enter the height initial height of the ball ")
+    velocity = input( "Please enter the initial velocity of the ball ")
+    isvalid(height, velocity)
+getinput()
+
+def isvalid(height, velocity):
+
+    if( (height <= 0) or (velocity <= 0) ):
+        print("Only positive values are accepted \n Please enter again")
+        getinput()
+    else:
+        maxheight(height,velocity)
+
+
+def maxheight(height,velocity):
+    equation = 
