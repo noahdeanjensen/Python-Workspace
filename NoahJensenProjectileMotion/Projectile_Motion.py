683a5d9631757086cfc264084b926a344c782036
Noah Jensen <noahj1247@gmail.com>
2023-03-18 00:06:03 -0600

    Main

--- NoahJensenProjectileMotion/Projectile_Motion.py
+++ NoahJensenProjectileMotion/Projectile_Motion.py
@@ -1,6 +1,12 @@
 """This program will calculate the height and also graph the projectile"""
+
 import numpy as np
 from sympy import *
+
+a = np.matrix('1 2; 3 4')
+
+print(a)
+
 
 def getinput():
     """This function takes in the input and passes the values to the isValid function """
@@ -15,8 +21,5 @@ def isvalid(height, velocity):
         print("Only positive values are accepted \n Please enter again")
         getinput()
     else:
-        maxheight(height,velocity)
+            print("hi")
 
-
-def maxheight(height,velocity):
-    equation = 
