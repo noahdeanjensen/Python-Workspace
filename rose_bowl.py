c2be7fc0c829be8d004fb11628d32d5762982f16
Noah Jensen <noahj1247@gmail.com>
2023-04-16 04:26:38 -0600

    Noah Jensen Final commit

--- rose_bowl.py
+++ rose_bowl.py
@@ -3,11 +3,11 @@
 from collections import Counter
 import os
 
+
 def main():
     """Main function for script"""
 
     def read_file():
-
         """using try/else statement read file into list """
         try:
             here = os.path.dirname(os.path.abspath(__file__))
@@ -18,22 +18,43 @@ def main():
 
             infile = open(filename, 'r', encoding="utf8")
 
+            # create list for teams
+
             teams = []
+
+            # read team in and add to list
 
             for line in infile:
                 teams.append(line.strip("\n"))
 
-            
+            infile.close()
 
-
+            return teams
 
         except FileNotFoundError:
 
             print("The file or directory cannot be found ")
 
+    def count_wins():
+        """Count the amount of wins from the file"""
 
+        teams = read_file()
 
+        # create dictionary for teams
 
-    read_file()
+        teams_dictionary = Counter(teams)
+
+        # find the teams that won 4 or more times.
+
+        for team, wins in sorted(teams_dictionary.items()):
+            if wins >= 4:
+                print(team, wins)
+
+    count_wins()
+
 
 main()
+
+"""I decided to go through and use the counter library to count the occurrences
+of the teams that had won. I didn't add any creative element but I was able to fulfill
+all other requirements."""
